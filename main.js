27068b95835a73cabbb6c8873baa1d5b9431d37c
Wioletta Gluza <wiolagluza@gmail.com>
2019-02-24 21:53:20 +0100

    General design finished, animation added

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 let box = document.querySelector('#box');
 let demo = document.querySelector('#demo');
 let icon = document.querySelector('#path-modified');
+let paragraph = document.querySelectorAll('.test');
 
 function runAnimation() {
   box.classList.add('colorTransition');
   demo.classList.add('pargraph-active');
   icon.classList.add('path-modified');
+  paragraph.forEach(function(p) {
+    p.classList.add('paragraph-active');
+  });
+
   console.log(`Animation started`);
 }
 
@@ -13,6 +18,9 @@ function initialState() {
   box.classList.remove('colorTransition');
   demo.classList.remove('pargraph-active');
   icon.classList.remove('path-modified');
+  paragraph.forEach(function(p) {
+    p.classList.remove('paragraph-active');
+  });
   console.log(`Initial State`);
 }
 
